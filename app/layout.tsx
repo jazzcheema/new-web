17f19c6216a810279892216fe5d5ff72e28a380b
Jazz Cheema <jaskamalc@gmail.com>
2024-06-03 18:03:37 -0700

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import ClientLayout from '@/components/ClientLayout';
 import { GlobalAudioProvider } from '@/components/GlobalAudio';
+import Head from 'next/head';
 
 export const metadata: Metadata = {
   title: 'Jazz Cheema',
